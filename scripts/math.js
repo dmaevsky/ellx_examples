062ad606c939629fef1b24acb376969ac01e0b71
Dmitry Maevsky <dmitry.maevsky@gmail.com>
2019-10-31 22:56:25 +0900

    MathJS patch: remove .type; rename patches to scripts

--- scripts/math.js
+++ scripts/math.js
@@ -1,8 +1,8 @@
 const ELLX_META = Symbol.for('~ellxMeta');
 
 export default function(math) {
-  math.type.Complex.prototype[ELLX_META] =
-  math.type.Matrix.prototype[ELLX_META] =
+  math.Complex.prototype[ELLX_META] =
+  math.Matrix.prototype[ELLX_META] =
   {
     operator: {
       binary: {
